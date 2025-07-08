14557a784b2a911de7594297b5837439c566740b
Ivan Ivanyas <ivanyasivan1@gmail.com>
2025-07-08 15:10:34 +0300

    lint

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,4 @@
-const getIndent = (depth) => '  '.repeat(depth)
+const getIndent = depth => '  '.repeat(depth)
 
 const stringify = (value, depth) => {
   if (typeof value !== 'object' || value === null) {
@@ -19,24 +19,24 @@ const iter = (tree, depth = 1) => {
   const bracketIndent = getIndent(depth - 1)
   return `{
 ${tree.map((node) => {
-    switch (node.type) {
-      case 'nested':
-        return `${indent}  ${node.key}: ${iter(node.children, depth + 1)}`
-      case 'added':
-        return `${indent}+ ${node.key}: ${stringify(node.value2, depth)}`
-      case 'removed':
-        return `${indent}- ${node.key}: ${stringify(node.value1, depth)}`
-      case 'updated':
-        return [
-          `${indent}- ${node.key}: ${stringify(node.value1, depth)}`,
-          `${indent}+ ${node.key}: ${stringify(node.value2, depth)}`,
-        ].join('\n')
-      case 'matched':
-        return `${indent}  ${node.key}: ${stringify(node.value1, depth)}`
-      default:
-        throw new Error(`Unknown node type: ${node.type}`)
-    }
-  }).join('\n')}
+  switch (node.type) {
+    case 'nested':
+      return `${indent}  ${node.key}: ${iter(node.children, depth + 1)}`
+    case 'added':
+      return `${indent}+ ${node.key}: ${stringify(node.value2, depth)}`
+    case 'removed':
+      return `${indent}- ${node.key}: ${stringify(node.value1, depth)}`
+    case 'updated':
+      return [
+        `${indent}- ${node.key}: ${stringify(node.value1, depth)}`,
+        `${indent}+ ${node.key}: ${stringify(node.value2, depth)}`,
+      ].join('\n')
+    case 'matched':
+      return `${indent}  ${node.key}: ${stringify(node.value1, depth)}`
+    default:
+      throw new Error(`Unknown node type: ${node.type}`)
+  }
+}).join('\n')}
 ${bracketIndent}}`
 }
 
